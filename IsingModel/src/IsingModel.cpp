eddd7ba73711676375460f6cb26c99e7da0b8cb8
Robin <robholt422@gmail.com>
2024-01-14 19:10:43 +0000

    Initial commit of project.

---
+++ IsingModel/src/IsingModel.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : IsingModel.cpp
+// Author      : Robin A. Holt
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Ising Model Git Test" << endl; // prints Ising Model
+	return 0;
+}
